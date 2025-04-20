3eacf9f6589f70fd8fe9cd3764c2ef8cab1bdbe8
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-04-20 16:09:39 +0600

    update menu responcive

--- src/main.js
+++ src/main.js
@@ -1,2 +1,36 @@
+import Swiper from 'swiper';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 import './style.css';
 
+const mainMenu = document.getElementById('menu');
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const closeIcon = document.getElementById('close-icon')
+
+hamburgerIcon.addEventListener('click', () =>{
+    mainMenu.classList.remove('hidden');
+    closeIcon.classList.remove('hidden');
+    hamburgerIcon.classList.add('hidden')
+})
+closeIcon.addEventListener('click', () =>{
+    mainMenu.classList.add('hidden');
+    hamburgerIcon.classList.remove('hidden')
+    closeIcon.classList.add('hidden')
+})
+
+
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Pagination],
+    spaceBetween: 30,
+    
+    autoplay: {
+        delay: 200,
+        
+      },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    loop: true,
+  });
