fce52585e4344f34c1a7c7e3178238a02dd2b31e
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-04-22 22:36:19 +0600

    update vite setting

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import { defineConfig } from 'vite';
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'about.html'),
+        service: path.resolve(__dirname, 'service.html'),
+        project: path.resolve(__dirname, 'project.html'),
+        blog: path.resolve(__dirname, 'blog.html'),
+        contact: path.resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
